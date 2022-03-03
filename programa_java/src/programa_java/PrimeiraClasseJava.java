7df456e146f81c08a7f641a2d0dee6bd21b04248
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 11:37:24 -0300

    atualização

--- programa_java/src/programa_java/PrimeiraClasseJava.java
+++ programa_java/src/programa_java/PrimeiraClasseJava.java
@@ -2,9 +2,8 @@ package programa_java;
 
 public class PrimeiraClasseJava {
 	
-	
-	boolean c = false;
-	int b = 20;
+	int a = 1;
+	boolean b = true;
 	
 	public static void main(String[] args) {
 		System.out.println("Ruinaldo Lopes de Souza 123");
