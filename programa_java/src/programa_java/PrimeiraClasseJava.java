cc5a1b7ca1c58699f3ad5af5521cb6b9e96eceda
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 10:42:49 -0300

    Comimit 4

--- programa_java/src/programa_java/PrimeiraClasseJava.java
+++ programa_java/src/programa_java/PrimeiraClasseJava.java
@@ -4,6 +4,7 @@ public class PrimeiraClasseJava {
 	
 	int a = 1;
 	boolean b = true;
+	boolean c = false;
 	
 	public static void main(String[] args) {
 		System.out.println("Ruinaldo Lopes de Souza 123");
