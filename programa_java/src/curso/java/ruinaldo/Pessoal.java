7a2d8caa7983d2fe49b7829dee2999285764b54b
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 11:13:22 -0300

    commit7

--- programa_java/src/curso/java/ruinaldo/Pessoal.java
+++ programa_java/src/curso/java/ruinaldo/Pessoal.java
@@ -11,6 +11,16 @@ public class Pessoal {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
 	
 	
 }
