64f27c099a0033bc3585ca32a3b0cda473afec54
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 10:14:53 -0300

    primeiro_Comit

--- programa_java/src/programa_java/PrimeiraClasseJava.java
+++ programa_java/src/programa_java/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package programa_java;
 
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		System.out.println("Hello teste");
+		System.out.println("Ruinaldo");
 	}
 
 }
