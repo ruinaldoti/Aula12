e5ba5e2402de1ef603552b4ffbb5b49aed315390
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 10:22:43 -0300

    commit 3

--- programa_java/src/programa_java/PrimeiraClasseJava.java
+++ programa_java/src/programa_java/PrimeiraClasseJava.java
@@ -3,6 +3,7 @@ package programa_java;
 public class PrimeiraClasseJava {
 	
 	int a = 1;
+	boolean b = true;
 	
 	public static void main(String[] args) {
 		System.out.println("Ruinaldo Lopes de Souza 123");
