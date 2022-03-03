aebf6fe3b057379224d6dabc2c67e107e46d86eb
Ruinaldo <ruinaldo.ti@gmail.com>
2022-03-03 11:04:39 -0300

    Classe_Pessoa

---
+++ programa_java/src/curso/java/ruinaldo/Pessoal.java
@@ -0,0 +1,15 @@
+package curso.java.ruinaldo;
+
+public class Pessoal {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
